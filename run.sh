7c8538bfc512ec64cfb1bfe53e9a41eeec6b92ee
Đặng Tiến Sĩ <dangtiensi.vn@gmail.com>
2023-01-03 17:11:15 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,11 +18,18 @@ fi
 
 echo "Current IP is $ip"
 
+if [ -f "./ip.log" ]; then
+    ip_old=$(cat ./ip.log)
+    if [[ ${ip} == ${ip_old} ]]; then
+        echo "Warning! Dynamic IP Has Not Changed"
+        exit 0
+    fi
+fi
+
 # get the dns record id
 dns_records_info=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records?type=A&name=$dns_record" \
     -H "Authorization: Bearer $token" \
     -H "Content-Type: application/json")
-# echo ${dns_records_info}
 dns_record_id=$(echo ${dns_records_info} | grep -o '"id":"[^"]*' | cut -d'"' -f4)
 if [[ -z "$dns_record_id" ]]; then
     echo "Error! Can't get '${dns_record}' record inforamiton from cloudflare API"
@@ -31,16 +38,17 @@ fi
 
 echo "DNS record id for '$dns_record' is '$dns_record_id'"
 
-
 # update the record
 update_dns_record=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_record_id" \
     -H "Authorization: Bearer $token" \
     -H "Content-Type: application/json" \
     --data "{\"type\":\"A\",\"name\":\"$dns_record\",\"content\":\"$ip\",\"ttl\":1,\"proxied\":false}")
-# echo ${update_dns_record}
 if [[ ${update_dns_record} != *"\"success\":true"* ]]; then
     echo "Error! Update Failed"
     exit 0
 fi
 
-echo "==> Success! $dns_record DNS Record Updated To: $ip"
+echo "Success! `$dns_record` DNS Record Updated To: `$ip`"
+
+# create file log
+echo -n "$ip" > ./ip.log
